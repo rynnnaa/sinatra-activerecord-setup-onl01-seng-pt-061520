58f1eb232fb52727cbf94c50b1c9f5e5c2e0d9bc
Ryna Truong <rynatruong@outlook.com>
2020-09-16 21:30:57 +0000

    Automatically backed up by Learn

--- db/migrate/20200916212928_create_dogs.rb
+++ db/migrate/20200916212928_create_dogs.rb
@@ -1,12 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
   end
-  
-  def up 
-  end
-  
+ 
   def down
+    drop_table :dogs
   end
-
-
 end
